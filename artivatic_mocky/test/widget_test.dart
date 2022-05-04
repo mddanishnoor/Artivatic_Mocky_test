e7a97483ef8d53f4dcdeb0b7c2b6c27091ea78bd
danish-innovationm <97957906+danish-innovationm@users.noreply.github.com>
2022-05-04 16:17:16 +0530

    Widget test_cases

--- artivatic_mocky/test/widget_test.dart
+++ artivatic_mocky/test/widget_test.dart
@@ -5,26 +5,27 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:artivatic_mocky/screens/list_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:artivatic_mocky/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('List screen search icon test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const Artivatic());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // Verify that our isSearchActive is false.
+    expect(find.byIcon(Icons.search), findsOneWidget);
+    expect(find.byIcon(Icons.cancel), findsNothing);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    // Tap the search icon and trigger a frame.
+    await tester.tap(find.byIcon(Icons.search));
     await tester.pump();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Verify that our isSearchActive is true.
+    expect(find.byIcon(Icons.search), findsNothing);
+    expect(find.byIcon(Icons.cancel), findsOneWidget);
   });
 }
